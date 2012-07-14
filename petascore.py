c109235dc7523552173672e40754def55609d1a6
ignaciocontreras <ignacio@nachovia.com>
2012-07-14 22:58:15 +0200

    Comments

--- petascore.py
+++ petascore.py
@@ -1,6 +1,7 @@
 import requests
 import lxml
 
+# It's "seems" a good idea to use this "enum", for now
 class Category:
     ALL = 0
     MOVIE = 1
@@ -11,10 +12,12 @@ class Category:
     TRAILER = 6
     COMPANY = 7
 
+# Unimplemented
 class Result:
     def __init__(self):
         self.things = ""
 
+# Contains info about the query to be made
 class Query:
     def __init__(self):
         self.category = Category.GAME
@@ -22,7 +25,13 @@ class Query:
         self.baseurl = "http://www.metacritic.com/search/"
         self.finalurl = self.baseurl + "/game/" + self.terms + "/results"
 
-    def mount_url(self):
+    def __init__(self, category, terms):
+        self.category = category
+        self.terms = terms
+        self.baseurl = "http://www.metacritic.com/search/"
+        self.finalurl = self.baseurl + "/game/" + self.terms + "/results"
+        
+    def get_url(self):
         partialurl = {Category.ALL: self.baseurl + "all",
                   Category.MOVIE: self.baseurl + "movie",
                   Category.GAME: self.baseurl + "game",
@@ -35,7 +44,7 @@ class Query:
         self.finalurl = partialurl + "/" + self.terms + "/results"
         return self.finalurl
     
-query = Query()
-print query.mount_url()
+query = Query(Category.GAME, "fallout")
+print query.get_url()
    
         
