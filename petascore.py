c838c299f517929b18e7def4dc8f081eddd26165
ignaciocontreras <ignacio@nachovia.com>
2012-07-15 02:45:50 +0200

    Query, Resource, Searcher schema

--- petascore.py
+++ petascore.py
@@ -33,7 +33,29 @@ class Query:
     def get_url(self):
         return self.url
 
-class Scrapper():
+# This class represents a generic resource found at Metascore, identified by
+# its URL
+class Resource:
+    def __init__(self, url):
+        self.url = url
+        self.get_data(url)
+        
+    def get_data(self):
+        req = requests.get(self.url)
+        if (req.status_code != 200):
+            # Something went wrong
+            return False
+        raw_data = req.content
+        # Parsing magic goes here!        
+        self.name = "..."
+        self.metascore = 0
+        self.userscore = 0
+        self.description = "..."
+    
+
+# This will be the main scrapping class. Its objective is to allow the
+# programmer to search directly through the Petascore module
+class Searcher():
     def __init__(self, query):
         self.raw_data = "Nothing yet"
         self.status_code = 0
@@ -43,11 +65,21 @@ class Scrapper():
         req = requests.get(self.query.get_url())
         self.status_code = req.status_code
         self.raw_data = req.content
+        
+    # def get_results(self):
+    #     ...
+    #     return infor about the results
+    
+    # def get_result_at(self, index);
+    #     ...
+    #     return some_url
+    
+    # ...
     
 # Basic development validation
 query = Query(Category.GAME, "fallout")
 print query.get_url()
-scrapper = Scrapper(query)
+scrapper = Searcher(query)
 scrapper.search()
 print scrapper.status_code
         
