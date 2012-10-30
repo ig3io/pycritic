33a90fcc60e671fe88d30f8d6a104762c7f8c491
ignaciocontreras <ignacio@nachovia.com>
2012-10-30 21:52:44 +0100

    Style and minor corrections. Add resource to demo.py

--- demo.py
+++ demo.py
@@ -7,12 +7,15 @@ def print_resource_data(resource):
     print "Userscore: " + str(resource.userscore)
     print "Description: " + resource.description
 
+
 def main():
     scraper = pycritic.Scraper()
     alien = scraper.get("http://www.metacritic.com/movie/alien")
     print_resource_data(alien)
     aliens = scraper.get("http://www.metacritic.com/movie/aliens")
     print_resource_data(aliens)
+    fallout = scraper.get("http://www.metacritic.com/game/pc/fallout-new-vegas")
+    print_resource_data(fallout)
 
 if __name__ == "__main__":
     main()
--- pycritic.py
+++ pycritic.py
@@ -20,15 +20,15 @@ class Query(object):
         self.terms = terms
         self.base_url = "http://www.metacritic.com/search/"
         partial_url = {Category.ALL: self.base_url + "all",
-                  Category.MOVIE: self.base_url + "movie",
-                  Category.GAME: self.base_url + "game",
-                  Category.ALBUM: self.base_url + "album",
-                  Category.TV: self.base_url + "tv",
-                  Category.PERSON: self.base_url + "person",
-                  Category.TRAILER: self.base_url + "trailer",
-                  Category.COMPANY: self.base_url + "company"}[self.category]
+                       Category.MOVIE: self.base_url + "movie",
+                       Category.GAME: self.base_url + "game",
+                       Category.ALBUM: self.base_url + "album",
+                       Category.TV: self.base_url + "tv",
+                       Category.PERSON: self.base_url + "person",
+                       Category.TRAILER: self.base_url + "trailer",
+                       Category.COMPANY: self.base_url + "company"}[self.category]
         self.url = partial_url + "/" + terms + "/results"
-        
+
     # Returns the URL of the created query
     def get_url(self):
         return self.url
@@ -47,35 +47,36 @@ class Resource(object):
 class Game(Resource):
     def __init__(self, name, date, category, metascore, userscore, description, platform):
         super.__init__(name, date, category, metascore, userscore, description)
-        self.platform = platform        
+        self.platform = platform
+
 
 class Response(object):
     def __init__(self, status, content):
         self.status = status
         self.content = content
-    
+
     def valid(self):
         return (self.status == 200)
-    
-    
+
+
 class Browser(object):
     def get(self, url):
         request = requests.get(url)
-        response  = Response(request.status_code, request.content)
+        response = Response(request.status_code, request.content)
         return response
-    
-        
+
+
 class Scraper(object):
-    def __init__(self, ):
+    def __init__(self):
         self.browser = Browser()
         self.response = ""
         self.soup = ""
-        
+
     def get(self, url):
         self.response = self.browser.get(url)
         self.soup = bs4.BeautifulSoup(self.response.content)
         return self.extract_data()
-    
+
     def extract_data(self):
         name = self._extract_name()
         date = self._extract_date()
@@ -83,31 +84,30 @@ class Scraper(object):
         metascore = self._extract_metascore()
         userscore = self._extract_userscore()
         description = self._extract_description()
-        #resource = Resource(name, date, category, metascore, userscore, description)
         resource = Resource(name, date, category, metascore, userscore, description)
         return resource
-    
+
     def _extract_name(self):
         titles = self.soup.select(".product_title")
         title = titles[0].text
         info = title.split("\n")
         name = info[1].strip()
         return name
-    
+
     def _extract_date(self):
         dates = self.soup.select(".release_data")
         date = dates[0].select(".data")[0].text.strip()
         return date
-    
+
     def _extract_category(self):
         # TODO
-        return Category.GAME 
-    
+        return Category.GAME
+
     def _extract_metascore(self):
         section = self.soup.select(".metascore_wrap")[0]
         score = section.select(".score_value")[0].text.strip()
         return int(score)
-    
+
     def _extract_userscore(self):
         section = self.soup.select(".userscore_wrap")[0]
         score = section.select(".score_value")[0].text.strip()
@@ -120,6 +120,6 @@ class Scraper(object):
         if (collapsed):  # There's a collapse/expand button
             expanded = section.select(".blurb_expanded")
             description = unicode(collapsed[0].text + expanded[0].text).strip()
-        else:    
+        else:
             description = unicode(section.text.strip())
         return unicode(description)
